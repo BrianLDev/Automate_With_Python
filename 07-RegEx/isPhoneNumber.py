ee373a53249f76bff6ca63204d29947843a36231
Brian Leip <brianleip@gmail.com>
2019-08-11 00:01:56 -0700

    got isPhoneNumber.py regex to work

--- 07-RegEx/isPhoneNumber.py
+++ 07-RegEx/isPhoneNumber.py
@@ -39,8 +39,8 @@ print("Done")
 
 
 ## REGEX VERSION
-import re 
-phoneNumRegex = re.compile('//d//d//d-//d//d//d-//d//d//d//d')
-mo = phoneNumRegex.search('My number is 415-555-4242.')
+import re
+phoneNumRegex = re.compile( r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search(' My number is 415-555-4242.')
 print('Phone number found: ' + mo.group() )
 
