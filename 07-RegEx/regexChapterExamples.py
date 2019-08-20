2f006c32e7a3ffdbb519ef3b41f3aeb9d04fc44f
Brian Leip <brianleip@gmail.com>
2019-08-19 23:16:33 -0700

    finished last of the regex chapter examples

--- 07-RegEx/regexChapterExamples.py
+++ 07-RegEx/regexChapterExamples.py
@@ -168,6 +168,19 @@ print(namesRegex.sub('REDACTED', "Agent Alice gave the secret documents to Agent
 agentNamesRegex = re.compile(r'Agent (\w)\w*')
 print("Agent Eve told Agent Carol that Agent Eve knew Agent Bob was a double agent.")
 print(agentNamesRegex.sub(r'\1****', "Agent Eve told Agent Carol that Agent Eve knew Agent Bob was a double agent."))
+print('*************')
 
 
 # Managing Complex Regexes
+phoneRegex = re.compile( r'''( 
+    (\ d{ 3} |\(\ d{ 3}\))?                 # area code 
+    (\ s |-|\.)?                            # separator 
+    \d{ 3}                                  # first 3 digits 
+    (\ s |-|\.)                             # separator 
+    \d{ 4}                                  # last 4 digits 
+    (\ s*( ext | x | ext.)\ s*\ d{ 2,5})?   # extension 
+    )''', re.VERBOSE) # re.VERBOSE ignores the whitespace above that is used to make it easier to format and visualize complex regexes
+
+# Combining re.IGNORECASE, re.DOTALL, and re.VERBOSE
+someRegexValue = re.compile(' foo', re.IGNORECASE | re.DOTALL)  # two options combined
+someRegexValue = re.compile(' foo', re.IGNORECASE | re.DOTALL | re.VERBOSE) # three options combined
