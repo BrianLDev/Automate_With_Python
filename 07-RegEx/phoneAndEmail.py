449fc181cd1a71f530a93490e6d89007c8f50e92
Brian Leip <brianleip@gmail.com>
2019-08-23 23:56:51 -0700

    added section to find matches in cliboard text in phoneAndEmail.py

--- 07-RegEx/phoneAndEmail.py
+++ 07-RegEx/phoneAndEmail.py
@@ -18,10 +18,19 @@ emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+_]+               # username
     @                               # @ symbol
     [a-zA-Z0-9.-]+                  # domain name
-    (\.[a-zA-Z])                    # dot-something
+    (\.[a-zA-Z]{2,4})                    # dot-something
     )''', re.VERBOSE)
  
-# TODO: Find matches in clipboard text. 
+# Find matches in clipboard text
+text = str(pyperclip.pasate())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
  
 # TODO: Copy results to the clipboard.
 
