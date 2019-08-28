6e512a30350b77b4c2f512d48747393601d6b344
Brian Leip <brianleip@gmail.com>
2019-08-28 10:12:33 -0700

    Created a loop that keeps checking for a strong password until it gets one in strongPW.py

--- 07-RegEx/strongPW.py
+++ 07-RegEx/strongPW.py
@@ -8,28 +8,31 @@ upperRegex = re.compile(r'[A-Z]+')
 lowerRegex = re.compile(r'[a-z]+')
 digitRegex = re.compile(r'\d+')
 
-print("\nEnter a password that you want to test to see if it is strong: ")
-password = input()
+strongPW = False
+while strongPW == False:
+    print("\nEnter a password that you want to test to see if it is strong: ")
+    password = input()
 
-lenChk = len(password) >= 8
-upperChk = upperRegex.search(password) != None
-lowerChk = lowerRegex.search(password) != None
-digitChk = digitRegex.search(password) != None
+    lenChk = len(password) >= 8
+    upperChk = upperRegex.search(password) != None
+    lowerChk = lowerRegex.search(password) != None
+    digitChk = digitRegex.search(password) != None
 
-if lenChk and upperChk and lowerChk and digitChk:
-    print("\nStrong Password!\n")
-else:
-    print("\nNot so Strong...")
-if not lenChk:
-    print("   *** Must be 8+ characters ***")
-if not upperChk:
-    print("   *** Must have at least 1 capital letter ***")
-if not lowerChk:
-    print("   *** Must have at least 1 lowercase letter ***")
-if not digitChk:
-    print("   *** Must have at least 1 digit ***")
+    if lenChk and upperChk and lowerChk and digitChk:
+        print("\nStrong Password!\n")
+        strongPW = True
+    else:
+        print("\nNot so Strong...")
+    if not lenChk:
+        print("   *** Must be 8+ characters ***")
+    if not upperChk:
+        print("   *** Must have at least 1 capital letter ***")
+    if not lowerChk:
+        print("   *** Must have at least 1 lowercase letter ***")
+    if not digitChk:
+        print("   *** Must have at least 1 digit ***")
 
-# Trying out Kyle from SPAR's code
-# KyleRegex = re.compile(r'^[A-Za-z0-9]{8}$')
-# KyleResult = KyleRegex.search(password) != None
-# print("\nKyle Regex Test = " + str(KyleResult) + "\n")
+    # Trying out Kyle from SPAR's code
+    # KyleRegex = re.compile(r'^[A-Za-z0-9]{8}$')
+    # KyleResult = KyleRegex.search(password) != None
+    # print("\nKyle Regex Test = " + str(KyleResult) + "\n")
