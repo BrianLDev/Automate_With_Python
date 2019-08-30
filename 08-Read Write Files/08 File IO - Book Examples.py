a452cb55f3400d90f276815d331794c681b42d4c
Brian Leip <brianleip@gmail.com>
2019-08-29 23:58:57 -0700

    started 08 File IO - Book Examples.py

---
+++ 08-Read Write Files/08 File IO - Book Examples.py
@@ -0,0 +1,7 @@
+# II - Automating Tasks
+## 08 - Read and Write Files
+#### Book Examples from Chapter
+
+# Backslack on Windows and Fwd Slash on OSX and Linux
+import os
+print(os.path.join('usr', 'bin', 'spam'))
