7fc1e28787bf98a4f83705d45e3f6ee90aa31dc8
Brian Leip <brianleip@gmail.com>
2020-08-27 16:26:26 -0700

    Improvements to th_scrape.py.  Will need to use selenium and PhantomJS to scrape dynamically loaded data

--- 11-Web Scraping/th_scrape.py
+++ 11-Web Scraping/th_scrape.py
@@ -2,23 +2,35 @@
 
 import requests, bs4
 
-# res = requests.get('https://tinyhouselistings.com/search?area_min=240&price_max=4500000&price_min=100&purchase_type=purchase&page=1')
-# try:
-#     res.raise_for_status()
-#     print("success")
-#     searchPage = bs4.BeautifulSoup(res.text, features="html.parser")
-#     print(searchPage.)
-# except Exception as exc:
-#     print('There was a problem: %s' % exc)
+def website_request(url, saveFileName=""):
+    website = requests.get(url)
+    try:
+        website.raise_for_status()
+        print("successfully loaded page: " + url)
+        if saveFileName != "":
+            with open(saveFileName, 'wb') as file:  # using with open() allows you to skip close() on the file after you're done with it
+                for chunk in website.iter_content(100000):
+                    file.write(chunk)
+        return website
+    except Exception as exc:
+        print('There was a problem: %s' % exc)
 
-res2 = requests.get('https://tinyhouselistings.com/listings/large-tiny-house-for-kids-tiny_house-mobile')
-try:
-    res2.raise_for_status()
-    print("success")
-    tinyHomePage = bs4.BeautifulSoup(res2.text, features="html.parser")
-    print(tinyHomePage.contents)
-    spans = tinyHomePage.select('div span')
-    print(len(spans))
-    print(spans[0])
-except Exception as exc:
-    print('Error: %s' % exc)
+
+url = 'https://tinyhouselistings.com/search?area_max=700&area_min=200&location_type=mobile&page=1&price_max=5000000&price_min=100&purchase_type=purchase'
+
+website = website_request(url, 'th_search.html')
+
+cards = website.select('.listing-card')
+print(cards)
+
+# res2 = requests.get('https://tinyhouselistings.com/listings/large-tiny-house-for-kids-tiny_house-mobile')
+# try:
+#     res2.raise_for_status()
+#     print("success")
+#     tinyHomePage = bs4.BeautifulSoup(res2.text, features="html.parser")
+#     print(tinyHomePage.contents)
+#     spans = tinyHomePage.select('div span')
+#     print(len(spans))
+#     print(spans[0])
+# except Exception as exc:
+#     print('Error: %s' % exc)
